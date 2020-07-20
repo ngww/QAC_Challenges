21ea6cacc8f361ae75930f245bf9cb1d596ab72e
ngww <ngww95@gmail.com>
2020-07-20 19:29:12 +0100

    Added pytest file

---
+++ Challenges/test_string.py
@@ -0,0 +1,12 @@
+import pytest
+from string_gen import string_gen
+
+def test_string_string():
+    assert type(string_gen()) is str
+
+def test_string_len():
+    assert len(string_gen()) == 5
+
+def test_string_lowercase():
+    string = string_gen()
+    assert string == string.lower()

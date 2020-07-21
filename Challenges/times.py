961d426ffcd5f3711dc735ba83c136a61a15a5b7
ngww <ngww95@gmail.com>
2020-07-21 08:12:47 +0100

    Updated times table function

--- Challenges/times.py
+++ Challenges/times.py
@@ -1,10 +1,10 @@
-def timestable(y):
-    x = 1
-    for i in range(1, y+1):
-        for i in range(1, y+1):
-            print(x*i, end='\t')
-        print("")
-        x+=1
+def timestable(n):
+    line = ""
+    for row in range(1, y+1):
+        for column in range(1, y+1):
+            line += str(row * column) + "\t"
+        line += "\n"
+    return line
 
 y = int(input("Enter a number: "))
-timestable(y)
+print(timestable(y))
